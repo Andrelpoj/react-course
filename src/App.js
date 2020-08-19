369074aedb12b9dd1eafa426d99ea46d598bc6fd
André Pereira <andrelpoj@gmail.com>
2020-08-18 22:40:00 -0300

    Conditionals

--- src/App.js
+++ src/App.js
@@ -28,27 +28,56 @@ class App extends Component {
         { name: 'Max', age: 28 },
         { name: event.target.value, age: 29 },
         { name: 'Stephanie', age: 28 },
-      ]
+      ],
+      otherState: 'some other value',
+      showPersons: false
     })
   }
 
+  togglPersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow});
+  }
+
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+            <Person 
+              name={this.state.persons[0].name} 
+              age={this.state.persons[0].age}>
+            </Person>
+            <Person 
+              name={this.state.persons[1].name} 
+              age={this.state.persons[1].age}
+              click={this.switchNameHandler}
+              changed={this.nameChangedHandler}> My Hobbies: Racing
+            </Person>
+            <Person name={this.state.persons[2].name} age={this.state.persons[2].age}></Person>
+          </div>
+      );
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App!</h1>
         <p>This is really working!</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person 
-          name={this.state.persons[0].name} 
-          age={this.state.persons[0].age}>
-        </Person>
-        <Person 
-          name={this.state.persons[1].name} 
-          age={this.state.persons[1].age}
-          click={this.switchNameHandler}
-          changed={this.nameChangedHandler}> My Hobbies: Racing
-        </Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}></Person>
+        <button 
+          style={style}
+          onClick={this.togglPersonsHandler}>
+          Toggl Persons
+        </button>
+        {persons}
       </div>
     );
   }
